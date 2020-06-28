0891ccfc8ce925b86ee17b6b31be0b8fbbd72895
park <dareme798@naver.com>
2020-06-29 02:09:13 +0900

    Import javax library

--- src/main/java/jyp/cooksite/domain/blog/blogs.java
+++ src/main/java/jyp/cooksite/domain/blog/blogs.java
@@ -2,10 +2,15 @@ package jyp.cooksite.domain.blog;
 
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 
 import jyp.cooksite.domain.user.User;
 import lombok.Getter;

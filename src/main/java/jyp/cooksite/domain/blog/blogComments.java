a0982f6bf018de7bf5e89bc5be07d48822ca1438
park <dareme798@naver.com>
2020-06-30 14:54:55 +0900

    Add Entity of blogComment.java

--- src/main/java/jyp/cooksite/domain/blog/blogComments.java
+++ src/main/java/jyp/cooksite/domain/blog/blogComments.java
@@ -1,5 +1,39 @@
 package jyp.cooksite.domain.blog;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import jyp.cooksite.domain.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class blogComments {
 
+	@Id @GeneratedValue
+	@Column(name = "blogcomment_id")
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "blogpost_id")
+	private blogPosts blogpost;
+	
+	private LocalDate createdDate;
+	
+	private LocalDate modDate;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	private String content;
+	
 }
